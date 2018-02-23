7ebccf9b83653f540004df0d6e0c1f156c5644e2
Lauro Moraes <lauromoraes.poa.rs@gmail.com>
2018-02-23 15:59:16 -0300

    small changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 /**
- * @license The MIT License (MIT)
- * @copyright Copyright (c) 2017 Lauro Moraes [https://github.com/subversivo58]
+ * @license The MIT License (MIT) - [https://github.com/subversivo58/subversivo58.github.io/blob/master/LICENSE]
+ * @copyright Copyright (c) 2017 Lauro Moraes - [https://github.com/subversivo58]
+ * @version 0.1.0 [development stage] - [https://github.com/subversivo58/subversivo58.github.io/blob/master/VERSIONING.md]
  */
  ;(function (root, factory) {
     if ( typeof define === 'function' && define.amd ) {
