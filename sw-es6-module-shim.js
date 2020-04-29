d2cc3b801f9f20b3345f16d5f06b1350c0cae257
Lauro Moraes <subversivo58@gmail.com>
2020-04-29 07:41:03 -0300

    add ServiceWorker file

---
+++ sw-es6-module-shim.js
@@ -0,0 +1,5 @@
+/**
+ * @license The MIT License (MIT)             - [https://github.com/subversivo58/importModules/blob/master/LICENSE]
+ * @copyright Copyright (c) 2020 Lauro Moraes - [https://github.com/subversivo58]
+ * @version 0.1.0 [development stage]         - [https://github.com/subversivo58/importModules/blob/master/VERSIONING.md]
+ */
