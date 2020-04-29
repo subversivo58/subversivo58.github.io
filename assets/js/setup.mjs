0ed56cb9eab305eedeaad6d3dbdbd052d1c9419c
Lauro Moraes <subversivo58@gmail.com>
2020-04-29 08:40:19 -0300

    update

--- assets/js/setup.mjs
+++ assets/js/setup.mjs
@@ -34,3 +34,66 @@ import {
     ServiceWorker,
     Promise
 } from './modules/BasePrefixes.mjs'
+import _ from './modules/Helpers.mjs'
+import {
+    Bytes2Size,
+    DeepFreeze,
+    DeepEqual,
+    StringDistance,
+    FindAndReplace,
+    Queue,
+    Stack,
+    PathName,
+    DirsLevel,
+    Redirect,
+    SendBeacon,
+    ColorPallet,
+    TimeLength,
+    TimeAgo
+} from './modules/Functions.mjs'
+import Emitter from './modules/Emitter.mjs'
+import AWIndexedDB from './modules/AwesomeIndexedDB.mjs'
+import BootstrapNative from './modules/bootstrap-native.mjs'
+import iLibJS from './modules/IlibJS.mjs'
+import {
+    Cookies as AWCookies,
+    Storage as AWStorage,
+    Caches as AWCache,
+    CleanBrowserStoragement as AWClearBrowserStoragement
+} from './modules/AwesomeStorage.mjs'
+
+const ConnectionDB = new AWIndexedDB('Subversivo58', 1, console.log)
+
+let BaseRootRegExp = new RegExp(BaseRoot),
+    redirectAfterSetup = (dc.referrer !== '') ? (dc.referrer.test(BaseRoot) ? dc.referrer : BaseRoot) : BaseRoot,
+    SettingsCFG
+
+new iLibJS('i18n')
+
+const Events = new Emitter('setup')
+
+// After HTMLDocument is charged and parsed [don't wait charge stylesheet's, images and subframe's]
+_.DOM.event(dc, 'DOMContentLoaded', async evt => {
+
+    const router = PathName(true)
+    switch (router) {
+
+        case 'index':
+            // check URI "search" fragment
+            if ( location.search === '?utm_source=homescreen' ) {
+                // is pwa launch ... register log
+            }
+            break
+
+        case 'dashboard':
+            break
+
+        case 'control':
+            break
+
+        case 'policy':
+            break
+
+    }
+
+}, _.EventOptions(false, true, true), true);
