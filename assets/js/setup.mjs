2344c8b430e0c7b4901a846eb07849e2a831384b
Lauro Moraes <subversivo58@gmail.com>
2021-02-07 02:05:01 -0300

    initial js files

---
+++ assets/js/setup.mjs
@@ -0,0 +1,5 @@
+/**
+ * @license The MIT License (MIT)             - [https://github.com/subversivo58/subversivo58.github.io/blob/master/LICENSE]
+ * @copyright Copyright (c) 2021 Lauro Moraes - [https://github.com/subversivo58]
+ * @version 0.1.0 [development stage]         - [https://github.com/subversivo58/subversivo58.github.io/blob/master/VERSIONING.md]
+ */
